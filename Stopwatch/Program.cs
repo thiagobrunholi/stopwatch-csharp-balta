e4e34286078abd60cf2f6b5799ba992a32561b75
Thiago Brunholi <thiago.brunholi@outlook.com>
2024-11-23 12:01:56 -0300

    Criando o menu
    Adicionando o menu da nossa aplicação.

--- Stopwatch/Program.cs
+++ Stopwatch/Program.cs
@@ -2,12 +2,20 @@
 {
     private static void Main(string[] args)
     {
-        Start();
+        Menu();
     }
 
-    static void Start()
+    static void Menu()
     {
-        int time = 10;
+        Console.Clear();
+        Console.WriteLine("S = Segundo");
+        Console.WriteLine("M = Minuto");
+        Console.WriteLine("0 = Sair");
+        Console.WriteLine("Quanto tempo deseja contar?");
+    }
+
+    static void Start(int time)
+    {
         int currentTime = 0;
 
         while (currentTime != time)
@@ -17,5 +25,9 @@
             Console.WriteLine(currentTime);
             Thread.Sleep(1000);
         }
+
+        Console.Clear();
+        Console.WriteLine("Stopwatch finalizado");
+        Thread.Sleep(2500);
     }
 }
--- Stopwatch/obj/Debug/net9.0/Stopwatch.AssemblyInfo.cs
+++ Stopwatch/obj/Debug/net9.0/Stopwatch.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Stopwatch")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a87c97f70218c10a5c3538b10a063999de1cc068")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bae61b6467261b42264ccdc1683e4b082cec5068")]
 [assembly: System.Reflection.AssemblyProductAttribute("Stopwatch")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Stopwatch")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
